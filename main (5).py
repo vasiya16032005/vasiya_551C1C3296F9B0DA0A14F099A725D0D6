7c9b8b22b2c5278fa338a43ba5db651af94dd341
vasiya16032005 <144215757+vasiya16032005@users.noreply.github.com>
2023-09-17 15:21:23 +0530

    Add files via upload

---
+++ main (5).py
@@ -0,0 +1,25 @@
+# Define the base class player
+class player:
+    def play(self):
+        print("The player is playing cricket.")
+
+ # Define the derived class Batsman
+class Batsman(player):
+     def play(self):
+         print("The batsman is batting.")
+
+# Define the derived class Bowler
+class Bowler(player):
+    def play (self):
+        print ("The bowler  is bowling.")
+
+# create objects of Batsman and bowler classes
+batsman = Batsman()
+bowler = Bowler()
+
+# call the play() method for each object
+batsman.play()
+bowler.play()
+
+
+  
